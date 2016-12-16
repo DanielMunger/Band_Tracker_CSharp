c3e91af05186b1e87ca42bd773064c24c4a53205
Daniel Munger <mungerda@gmail.com>
2016-12-16 08:41:33 -0800

    add test for empty database. not passing.

--- Tests/VenueTest.cs
+++ Tests/VenueTest.cs
@@ -7,17 +7,26 @@ using System.Data.SqlClient;
 
 namespace  BandTracker.Tests
 {
-  public class TEMPLATETest : IDisposable
+  public class VenueTest : IDisposable
   {
-    public TEMPLATETest()
+    public VenueTest()
     {
       DBConfiguration.ConnectionString = "Data Source=DESKTOP-GC3DC7B\\SQLEXPRESS;Initial Catalog=band_tracker_test;Integrated Security=SSPI;";
     }
-
     [Fact]
-    public void TEMPLATE_true()
+    public void GetAll_ReturnsEmptyList_True()
+    {
+      int result = Venue.GetAll().Count;
+      int expected = 0;
+
+      Assert.Equal(expected, result);
+    }
+    [Fact]
+    public void Save_SavesVenueToDatabase_true()
     {
       //Arrange
+      Venue newVenue = new Venue("Belly Up", 455, "213 E. Durant");
+      newVenue.Save();
       //Act
       //Assert
       Assert.Equal(true/false, TEMPLATE);
