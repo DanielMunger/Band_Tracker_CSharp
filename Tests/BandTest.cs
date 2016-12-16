547f7b55a3a56b197d1dbda44537f621b4825593
Daniel Munger <mungerda@gmail.com>
2016-12-16 10:58:31 -0800

    add passing method and test for getall.

--- Tests/BandTest.cs
+++ Tests/BandTest.cs
@@ -1,32 +1,33 @@
-// using Xunit;
-// using System;
-// using System.Collections.Generic;
-// using BandTracker.Objects;
-// using System.Data;
-// using System.Data.SqlClient;
-//
-// namespace  BandTracker.Tests
-// {
-//   public class TEMPLATETest : IDisposable
-//   {
-//     public TEMPLATETest()
-//     {
-//       DBConfiguration.ConnectionString = "Data Source=DESKTOP-GC3DC7B\\SQLEXPRESS;Initial Catalog=band_tracker_test;Integrated Security=SSPI;";
-//     }
-//
-//     [Fact]
-//     public void TEMPLATE_true()
-//     {
-//       //Arrange
-//       //Act
-//       //Assert
-//       Assert.Equal(true/false, TEMPLATE);
-//     }
-//
-//     public void Dispose()
-//     {
-//       TEMPLATE_OBJECT.DeleteAll(); //TEMPLATE_OBJECT references the object name
-//     }
-//
-//   }
-// }
+using Xunit;
+using System;
+using System.Collections.Generic;
+using BandTracker.Objects;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace  BandTracker.Tests
+{
+  public class BandTest : IDisposable
+  {
+    public BandTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=DESKTOP-GC3DC7B\\SQLEXPRESS;Initial Catalog=band_tracker_test;Integrated Security=SSPI;";
+    }
+
+    [Fact]
+    public void GetAll_ReturnsEmptyList_True()
+    {
+      int result = Band.GetAll().Count;
+      int expected = 0;
+
+      Assert.Equal(expected, result);
+    }
+  //  Band newBand = new Band("Kings of Leon", "Indie Rock");
+
+    public void Dispose()
+    {
+      Band.DeleteAll();
+    }
+
+  }
+}
