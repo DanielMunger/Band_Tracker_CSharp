7f9393494a7f133397df2c0f8243527d303270fd
Daniel Munger <mungerda@gmail.com>
2016-12-16 14:15:28 -0800

    add basic view routes.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,18 +1,34 @@
-// using Nancy;
-// using System.Collections.Generic;
-// using System;
-// using TEMPLATE.Objects;
-//
-// namespace TEMPLATE
-// {
-//   public class HomeModule : NancyModule
-//   {
-//     public HomeModule()
-//     {
-//       Get["/"] = _ => {
-//         return View["TEMPLATE.cshtml"];
-//       };
-//
-//     }
-//   }
-// }
+using Nancy;
+using System.Collections.Generic;
+using System;
+using BandTracker.Objects;
+
+namespace BandTracker
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+      Get["/bands"] = _ => {
+        List<Band> allBands = Band.GetAll();
+        return View["bands.cshtml", allBands];
+      };
+      Get["/venues"] = _ => {
+        List<Venue> allVenues = Venue.GetAll();
+        return View["venues.cshtml", allVenues];
+      };
+      Get["/band/new"] = _ => {
+        return View["new_band_form.cshtml"];
+      };
+      // Post["band/new"] = _ => {
+      //
+      // };
+      Get["/venue/new"] = _ => {
+        return View["new_venue_form.cshtml"];
+      };
+    }
+  }
+}
